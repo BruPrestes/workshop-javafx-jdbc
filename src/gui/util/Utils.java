a13d38ce68b930e752e4cbaf1bd2ccaf889c8378
Bruno <brunnoprestes@gmail.com>
2021-05-03 01:44:45 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -11,6 +11,13 @@ public class Utils {
 		return (Stage)((Node) event.getSource()).getScene().getWindow();
 	}
 
-
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
 }
 
